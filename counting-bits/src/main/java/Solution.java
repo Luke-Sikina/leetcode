36341032e4f49a5d3e85cdc4c98e698b055d49c3
Luke Sikina <lucas@ds.dfci.harvard.edu>
2022-03-01 09:47:11 -0500

    counting bits

---
+++ counting-bits/src/main/java/Solution.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] countBits(int n) {
+        return IntStream.range(0, n + 1)
+            .map(Integer::bitCount)
+            .toArray();
+    }
+}
