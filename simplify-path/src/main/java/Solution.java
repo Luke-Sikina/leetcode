39a6aedbb5563e47a63500b2bc04dfd5f4e252eb
Luke Sikina <lucas@ds.dfci.harvard.edu>
2022-03-14 11:51:08 -0400

    switch statement reads a bit better

--- simplify-path/src/main/java/Solution.java
+++ simplify-path/src/main/java/Solution.java
@@ -5,15 +5,18 @@ class Solution {
     public String simplifyPath(String path) {
         List<String> processedNodes = new ArrayList<>();
         for (String node : path.split("/")) {
-            if (node.isEmpty() || ".".equals(node)) {
-                continue;
-            } else if ("..".equals(node)) {
-                if (!processedNodes.isEmpty()) {
-                    // go back 1 dir
-                    processedNodes.remove(processedNodes.size() - 1);
-                }
-            } else {
-                processedNodes.add(node);
+            switch (node) {
+                case "":
+                case ".":
+                    continue;
+                case "..":
+                    if (!processedNodes.isEmpty()) {
+                        // go back 1 dir
+                        processedNodes.remove(processedNodes.size() - 1);
+                    }
+                    continue;
+                default:
+                    processedNodes.add(node);
             }
         }
 
