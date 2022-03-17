7355c7a96a4a822ea0af0aac0a5fd061a08295f9
Luke Sikina <lucas@ds.dfci.harvard.edu>
2022-03-17 11:29:12 -0400

    score-of-paren

---
+++ score-of-parentheses/src/main/java/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class Solution {
+    public int scoreOfParentheses(String S) {
+        Stack<Integer> stack = new Stack<>();
+        int cur = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(') {
+                stack.push(cur);
+                cur = 0;
+            } else {
+                cur = stack.pop() + Math.max(cur * 2, 1);
+            }
+        }
+        return cur;
+    }
+}
