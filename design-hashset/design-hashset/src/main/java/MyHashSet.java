683138a99c325205aaeb08bde950654e38ffb019
Luke Sikina <lucas@ds.dfci.harvard.edu>
2022-04-21 10:52:35 -0400

    hash-set

---
+++ design-hashset/design-hashset/src/main/java/MyHashSet.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class MyHashSet {
+    private LinkedList<Integer>[] table;
+    private int tableSize;
+    private int entries;
+    private static final int STARTING_TABLE_SIZE = 16;
+
+    public MyHashSet() {
+        table = new LinkedList[STARTING_TABLE_SIZE];
+        tableSize = STARTING_TABLE_SIZE;
+        entries = 0;
+    }
+
+    public void add(int key) {
+        if (entries == tableSize) {
+            adjustTableSize(tableSize * 2);
+        }
+
+        int resizedKey = resizeKey(key);
+        LinkedList<Integer> head = table[resizedKey];
+        if (head == null) {
+            head = new LinkedList<>(List.of(key));
+            table[resizedKey] = head;
+            entries++;
+        } else if (!head.contains(key)){
+            head.add(key);
+            entries++;
+        }
+    }
+
+    private void adjustTableSize(int newSize) {
+        tableSize = newSize;
+        LinkedList<Integer>[] oldTable = table;
+        table = new LinkedList[tableSize];
+
+        for (LinkedList<Integer> integers : oldTable) {
+            if (integers != null) {
+                integers.forEach(this::add);
+            }
+        }
+    }
+
+    private int resizeKey(int key) {
+        return key & (table.length - 1);
+    }
+
+    public void remove(int key) {
+        int resizedKey = resizeKey(key);
+        LinkedList<Integer> head = table[resizedKey];
+        if (head != null && head.remove(Integer.valueOf(key))) {
+            entries--;
+        }
+        if (entries == tableSize / 2 && tableSize > STARTING_TABLE_SIZE) {
+            adjustTableSize(tableSize / 2);
+        }
+
+    }
+
+    public boolean contains(int key) {
+        LinkedList<Integer> head = table[resizeKey(key)];
+        return head != null && head.contains(key);
+    }
+}
